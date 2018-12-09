ee974ffa0b1951eec7b206474b3c37bac468a813
Minh Nguyen Duc <nguyenducminh24101998@gmail.com>
2018-12-09 23:46:01 +0700

    submit

---
+++ Localization/Data/LocalizationContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Localization.Models
+{
+    public class LocalizationContext : DbContext
+    {
+        public LocalizationContext (DbContextOptions<LocalizationContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Localization.Models.User> User { get; set; }
+    }
+}
